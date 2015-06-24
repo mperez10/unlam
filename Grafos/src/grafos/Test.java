0dfeb03ea877184f790f59d1155c1872fe75cb0c
Franco <ezefran256@gmail.com>
2015-06-24 17:53:47 -0300

    Update Test.java

--- Grafos/src/grafos/Test.java
+++ Grafos/src/grafos/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		int inf = Grafo.INFINITO;
 		int matriz[][] = {  {0, 2, 4},
-							{inf, 0, 5},
-							{inf, inf, 0}  };
+				    {inf, 0, 5},
+				    {inf, inf, 0}  };
 		Grafo g = new Grafo(matriz);
 //		g.DFS(g.getNodo(0));
 		g.BFS(g.getNodo(0));
