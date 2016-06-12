b695d64565240e8c39e03b244604d3cf9c3183ac
Franco Falleti <ezefran256@gmail.com>
2016-06-12 19:28:07 -0300

    Fork corregido

--- Sistemas Operativos/TP3/Ejercicio 1/Proceso Pesado/Caso1.c
+++ Sistemas Operativos/TP3/Ejercicio 1/Proceso Pesado/Caso1.c
@@ -51,48 +51,49 @@ int caso1(int *array) {
     time_t t_total, t_prom;
     long double sprom, uprom;
     struct rusage usage, acum;
-    int padre = 1;
     pid_t pid[CANT_PROCESOS];
     clock_gettime(CLOCK_MONOTONIC_RAW, &inicio);
 
-    for (int i = 0; i < CANT_PROCESOS; i++) {
+    for (int i = 0; i < CANT_PROCESOS; i++)
+    {
         pid[i] = fork();
-        switch( pid[i] ) {
-            case -1: // Error
-                break;
-            case 0: // Hijo
-                lectura( &(*array) );
-                i = CANT_PROCESOS;
-                padre = 0;
-                break;
-            default: // Padre
-                inicializarUsage(&acum);
-                for (int j = 0; j < CANT_PROCESOS; j++) {
-                    wait3(NULL, 0, &usage);
-                    acum.ru_stime.tv_usec += usage.ru_stime.tv_usec;
-                    acum.ru_utime.tv_usec += usage.ru_utime.tv_usec;
-                    acum.ru_minflt += usage.ru_minflt;
-                    acum.ru_majflt += usage.ru_majflt;
-                    acum.ru_nsignals += usage.ru_nsignals;
-                    acum.ru_nvcsw += usage.ru_nvcsw;
-                    acum.ru_nivcsw += usage.ru_nivcsw;
-                }
-                break;
+        if (pid[i] == 0) // Proceso Hijo
+        {
+            lectura( &(*array) );
+            i = CANT_PROCESOS;
+            exit(0);
+        }
+        else if (pid[i] == -1)
+        {
+            printf("Error creando proceso\n");
+            exit(1);
         }
     }
 
-    if (padre == 1)
-    {
-        clock_gettime(CLOCK_MONOTONIC_RAW, &fin);
-        t_total = fin.tv_nsec - inicio.tv_nsec; // nanosegundos
-        if (t_total < 0)
-            t_total += 1000000000;
-        t_total = t_total / 1000 + 1000000*(fin.tv_sec - inicio.tv_sec); // convierte a microsegundos
-        t_prom = t_total / CANT_PROCESOS; // se desprecia un valor infimo
-        sprom = acum.ru_stime.tv_usec / CANT_PROCESOS;
-        uprom = acum.ru_utime.tv_usec / CANT_PROCESOS;
-        imprimir(&acum, &t_total, &t_prom, &sprom, &uprom);
+    // Continua el proceso Padre
+
+    inicializarUsage(&acum);
+    for (int j = 0; j < CANT_PROCESOS; j++) {
+        wait3(NULL, 0, &usage);
+        acum.ru_stime.tv_usec += usage.ru_stime.tv_usec;
+        acum.ru_utime.tv_usec += usage.ru_utime.tv_usec;
+        acum.ru_minflt += usage.ru_minflt;
+        acum.ru_majflt += usage.ru_majflt;
+        acum.ru_nsignals += usage.ru_nsignals;
+        acum.ru_nvcsw += usage.ru_nvcsw;
+        acum.ru_nivcsw += usage.ru_nivcsw;
     }
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &fin);
+    t_total = fin.tv_nsec - inicio.tv_nsec; // nanosegundos
+    if (t_total < 0)
+        t_total += 1000000000;
+    t_total = t_total / 1000 + 1000000*(fin.tv_sec - inicio.tv_sec); // convierte a microsegundos
+    t_prom = t_total / CANT_PROCESOS; // se desprecia un valor infimo
+    sprom = acum.ru_stime.tv_usec / CANT_PROCESOS;
+    uprom = acum.ru_utime.tv_usec / CANT_PROCESOS;
+    imprimir(&acum, &t_total, &t_prom, &sprom, &uprom);
+
     return 0;
 }
 
